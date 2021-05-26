3b6d0b72345dcdd78c14db11da5a8a45e5103772
Humberto <humbhenri@gmail.com>
2021-05-26 13:43:26 -0300

    first commit

---
+++ src/main/java/com/humbhenri/jtwitter/JtwitterApplication.java
@@ -0,0 +1,13 @@
+package com.humbhenri.jtwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humbhenri/jtwitter/JtwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humbhenri.jtwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
