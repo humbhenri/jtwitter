7a96979e03899f601be700f2105d701d6c8fc304
Humberto <humbhenri@gmail.com>
2021-05-28 16:36:29 -0300

    tweetbox calls api

--- react-app/src/API.js
+++ react-app/src/API.js
@@ -3,13 +3,25 @@ import axios from 'axios';
 // TODO replace fixed token with a login
 const server = axios.create({
     baseURL: 'http://localhost:8080/',
-    headers: { Authorization : 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJIdW1iZXJ0byIsImF1dGgiOlt7ImF1dGhvcml0eSI6IlVTRVIifV0sImlhdCI6MTYyMjIyOTI2MiwiZXhwIjoxNjIyMjMyODYyfQ.DTh_qa-mlelsdBwIZBSY6W5Ok4YIQyyznOU262the3s'}
+    headers: {
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJIdW1iZXJ0byIsImF1dGgiOlt7ImF1dGhvcml0eSI6IlVTRVIifV0sImlhdCI6MTYyMjIyOTI2MiwiZXhwIjoxNjIyMjMyODYyfQ.DTh_qa-mlelsdBwIZBSY6W5Ok4YIQyyznOU262the3s',
+    },
 });
 
 export async function getPosts() {
     try {
         const res = await server.get('posts');
         return res.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function tweet({ text, urlImage }) {
+    try {
+        const res = server.post('posts', { text, urlImage });
+        console.log('tweet successfull');
     } catch (error) {
         console.error(error);
     }
--- react-app/src/TweetBox.js
+++ react-app/src/TweetBox.js
@@ -1,20 +1,41 @@
-import { Button, Avatar } from '@material-ui/core'
-import React from 'react'
-import './TweetBox.css'
+import { Button, Avatar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { tweet } from './API';
+import './TweetBox.css';
 
 export default function TweetBox() {
+    const [text, setText] = useState('');
+    const [urlImage, setUrlImage] = useState('');
+    const sendTweet = async (e) => {
+        e.preventDefault();
+        await tweet({text, urlImage});
+        setText('');
+        setUrlImage('');
+    };
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar>HP</Avatar>
-                    <input type="text" placeholder="What's happening ?" />
+                    <input
+                        type="text"
+                        placeholder="What's happening ?"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                    />
                 </div>
                 <div className="tweetBox__inputImg">
-                    <input type="text" placeholder="Optional: Post a image url" />
+                    <input
+                        type="text"
+                        placeholder="Optional: Post a image url"
+                        value={urlImage}
+                        onChange={(e) => setUrlImage(e.target.value)}
+                    />
                 </div>
-                <Button className="tweetBox__tweetButton">Tweet</Button>
+                <Button className="tweetBox__tweetButton" onClick={sendTweet}>
+                    Tweet
+                </Button>
             </form>
         </div>
-    )
+    );
 }
