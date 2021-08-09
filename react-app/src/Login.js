02571bb4589666dc8abcf5b82dc82b265f2b8b9a
Humberto Pinheiro <humbhenri@gmail.com>
2021-08-09 19:47:51 -0300

    fix signup bugs

--- react-app/src/Login.js
+++ react-app/src/Login.js
@@ -1,44 +1,59 @@
-import React, { useState } from 'react';
-import { login } from './API';
-import './Login.css';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import Signup from './Signup';
+import React, { useState } from "react";
+import { login } from "./API";
+import "./Login.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import Signup from "./Signup";
 
 export default function Login({ setToken }) {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const handleSubmit = async e => {
-        setError('');
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const clear = () => {
+        setName("");
+        setPassword("");
+        setError("");
+    };
+    const handleSubmit = async (e) => {
+        setError("");
         e.preventDefault();
         const token = await login({ name, password });
         if (token) {
             console.log(token);
             setToken(token);
-            setName('');
-            setPassword('');
-            setError('');
+            clear();
         } else {
-            setError('Login or password incorrect');
+            setError("Login or password incorrect");
         }
-    }
+    };
     return (
         <div className="login">
-            <TwitterIcon className="login__twitterIcon"/>
+            <TwitterIcon className="login__twitterIcon" />
             <h2 className="login__title">Login to JTwitter</h2>
-            <div className="login__error">{error && <p className="login__error">{error}</p>}</div>
+            <div className="login__error">
+                {error && <p className="login__error">{error}</p>}
+            </div>
             <form onSubmit={handleSubmit}>
                 <div className="login__input">
-                    <input type="text" placeholder="User name"
-                        onChange={e => setName(e.target.value)} value={name} required/>
+                    <input
+                        type="text"
+                        placeholder="User name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        required
+                    />
                 </div>
                 <div className="login__input">
-                    <input type="password" placeholder="Password"
-                        onChange={e => setPassword(e.target.value)} value={password} required/>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                    />
                 </div>
                 <button className="login__submit">Log in</button>
-                <Signup></Signup>
+                <Signup onModalOpen={() => clear()}></Signup>
             </form>
         </div>
-    )
+    );
 }
--- react-app/src/Signup.js
+++ react-app/src/Signup.js
@@ -1,74 +1,106 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import './Signup.css';
-import { signup } from './API';
+import React, { useState } from "react";
+import Modal from "react-modal";
+import "./Signup.css";
+import { signup } from "./API";
 
 const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
-export default function Signup() {
+export default function Signup(props) {
     const [modalIsOpen, setIsOpen] = useState(false);
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [displayName, setDisplayName] = useState('');
-    const [error, setError] = useState('');
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [error, setError] = useState("");
     function openModal() {
         setIsOpen(true);
+        if (props.onModalOpen) {
+            props.onModalOpen();
+        }
     }
     function closeModal() {
-        setError('');
-        setName('');
-        setPassword('');
-        setDisplayName('');
+        setError("");
+        setName("");
+        setPassword("");
+        setDisplayName("");
         setIsOpen(false);
     }
     async function doSignup(e) {
-        setError('');
+        if (!name || !password || !displayName) {
+            setError("Required name, password and display name not informed");
+            return;
+        }
+        setError("");
         const data = await signup({ name, password, displayName });
-        if (data && 'message' in data) {
+        if (data && "message" in data) {
             setError(data.message);
         } else {
-            console.log('Signup successful');
+            console.log("Signup successful");
             closeModal();
         }
     }
     return (
-      <>
-      <button onClick={openModal}  className="signup__open">Sign up</button>
-        <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Signup"
-        >
-        <h2>Sign up on JTwitter</h2>
-        <div className="login__error">{error && <p className="login__error">{error}</p>}</div>
-        <form>
-            <div className="login__input">
-                <input type="text" placeholder="User name"
-                    onChange={e => setName(e.target.value)} value={name} required/>
-            </div>
-            <div className="login__input">
-                <input type="text" placeholder="Display name"
-                    onChange={e => setDisplayName(e.target.value)} value={displayName} required/>
-            </div>
-            <div className="login__input">
-                <input type="password" placeholder="Password"
-                    onChange={e => setPassword(e.target.value)} value={password} required/>
-            </div>
-            <button type="button" className="login__submit" onClick={doSignup}>Create account</button>
-        </form>
-      </Modal>
-      </>
-    )
+        <>
+            <button onClick={openModal} className="signup__open">
+                Sign up
+            </button>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Signup"
+            >
+                <h2>Sign up on JTwitter</h2>
+                <div className="login__error">
+                    {error && <p className="login__error">{error}</p>}
+                </div>
+                <form>
+                    <div className="login__input">
+                        <input
+                            type="text"
+                            placeholder="User name"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                            required
+                        />
+                    </div>
+                    <div className="login__input">
+                        <input
+                            type="text"
+                            placeholder="Display name"
+                            onChange={(e) => setDisplayName(e.target.value)}
+                            value={displayName}
+                            required
+                        />
+                    </div>
+                    <div className="login__input">
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            required
+                        />
+                    </div>
+                    <button
+                        type="button"
+                        className="login__submit"
+                        onClick={doSignup}
+                    >
+                        Create account
+                    </button>
+                </form>
+            </Modal>
+        </>
+    );
 }
